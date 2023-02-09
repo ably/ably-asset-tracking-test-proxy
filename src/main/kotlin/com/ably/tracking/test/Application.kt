a81d74b46fe2f7c95b886aa75491ec2f7926b124
Lawrence Forooghian <lawrence@forooghian.com>
2023-02-09 15:34:12 -0300

    Switch off trace logging, and log requests

--- src/main/kotlin/com/ably/tracking/test/Application.kt
+++ src/main/kotlin/com/ably/tracking/test/Application.kt
@@ -6,6 +6,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 
 fun main() {
@@ -17,5 +18,6 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+    install(CallLogging)
     configureRouting()
 }
