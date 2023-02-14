34e57dbe76d2f4a67a1fff820639826cdf162e86
Lawrence Forooghian <lawrence@forooghian.com>
2023-02-14 03:40:05 -0300

    Set up ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.3"
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
 }
 
 group = "com.ably.tracking.test"
